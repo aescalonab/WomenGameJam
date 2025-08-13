c607fb1207a45aa68af4ef356eb1107ae0a10bbe
IanMonday <iansanti052@gmail.com>
2025-08-13 01:20:42 -0500

    Mejoras Fisicas Nivel 1

---
+++ Jam - copia/Assets/Guardado.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Guardado",menuName = "Guardado")]
+public class Guardado : ScriptableObject
+{
+    public bool transicion;
+    public bool teclas;
+}
